797b5d27a03ac02232060d6eacc87c8f3e26b7be
Tyler Mayoff <tyler@tylermayoff.com>
2022-10-10 20:50:41 -0400

    printing some log information

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+#[macro_use]
+extern crate log;
+
+use std::path::Path;
+
 use clap::Parser;
+use chrono::prelude::*;
 
 #[derive(Debug, Clone, clap::ValueEnum)]
 enum Commands {
     Setup,
     Compile
 }
-
 
 #[derive(Parser)]
 struct CliArgs {
@@ -16,12 +21,25 @@ struct CliArgs {
     build_dir: String
 }
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
+    env_logger::init();
+
     let args = CliArgs::parse();
+    
+    let binding = std::env::current_dir().expect("Can't get source directory");
+    let source_directory = binding.as_path();
 
     match args.command {
         Commands::Setup => {
-            
+            let build_directory = source_directory.join(args.build_dir);
+
+            debug!("Build Started at {}", Local::now());
+            info!("Reson Build System");
+            info!("Version: {}", VERSION);
+            info!("Source Directory {:?}", source_directory.to_str());
+            info!("Build Directory {:?}", build_directory.to_str());
         },
         Commands::Compile => {
 
--- tests/simple.rs
+++ tests/simple.rs
@@ -5,8 +5,13 @@ use assert_cmd::prelude::*;
 fn simple_test() -> Result<(), Box<dyn std::error::Error>>{
     let mut cmd = Command::cargo_bin("reson")?;
 
-    cmd.arg("setup").arg("-C").arg(" build").current_dir(fs::canonicalize("tests/build_scripts/simple")?);
+    cmd.arg("setup").arg("-C").arg("build").current_dir(fs::canonicalize("tests/build_scripts/simple")?);
     cmd.assert().success();
+
+    let stdout = &cmd.output()?.stdout;
+    let stderr = cmd.output()?.stderr;
+    println!("--- stdout\n{}", std::str::from_utf8(stdout).unwrap());
+    println!("--- stderr\n{}", std::str::from_utf8(&stderr).unwrap());
 
     Ok(())
 }
