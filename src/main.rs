9244d64c9545960684fad56cc9079d15bbd6d706
Tyler Mayoff <tyler@tylermayoff.com>
2022-10-11 20:44:33 -0400

    Setup now adds files

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,129 @@
 #[macro_use]
 extern crate log;
 
-use std::path::Path;
+use snafu::{prelude::*, Whatever};
+use std::{env::current_dir, path::Path, path::PathBuf};
 
-use clap::Parser;
 use chrono::prelude::*;
+use clap::{Parser, Subcommand};
 
-#[derive(Debug, Clone, clap::ValueEnum)]
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const BUILD_FILE_NAME: &str = "meson.build";
+
+fn has_build_file(dir: &PathBuf) -> bool {
+    let mut d = PathBuf::new();
+    d.push(dir);
+    d.push(BUILD_FILE_NAME);
+    d.exists()
+}
+
+fn validate_core_dirs(
+    dir1: Option<PathBuf>,
+    dir2: Option<PathBuf>,
+) -> Result<(PathBuf, PathBuf), Whatever> {
+    let mut _dir1 = PathBuf::new();
+    let mut _dir2 = PathBuf::new();
+    match dir1 {
+        Some(dir1) => {
+            _dir1 = dir1;
+        }
+        None => {
+            if dir2.is_none() {
+                if !Path::new("meson.build").exists() && Path::new("../meson.build").exists() {
+                    _dir2.push("..");
+                } else {
+                    whatever!("Must specify at least one directory")
+                }
+            }
+            let cwd = std::env::current_dir().expect("Failed to get current working directory");
+            _dir1 = cwd;
+        }
+    }
+
+    if dir2.is_none() {
+        _dir2 = current_dir().expect("Failed to get current working directory");
+    }
+
+    // let _dir1 = _dir1
+    //     .canonicalize()
+    //     .expect("Failed to create absolute path");
+    // let _dir2 = _dir2
+    //     .canonicalize()
+    //     .expect("Failed to create absolute path");
+
+    let err = std::fs::create_dir_all(&_dir1);
+    if let Err(e) = err {
+        error!("Failed to create directories {}", e);
+    }
+
+    let err = std::fs::create_dir_all(&_dir2);
+    if let Err(e) = err {
+        error!("Failed to create directories {}", e);
+    }
+
+    // TODO Make sure the dirs are not the same
+
+    if has_build_file(&_dir1) {
+        if has_build_file(&_dir2) {
+            whatever!("Both directories contain a build file.")
+        }
+
+        return Ok((_dir1, _dir2));
+    }
+
+    if has_build_file(&_dir2) {
+        return Ok((_dir2, _dir1));
+    }
+
+    whatever!("Neither directory contains a build file")
+}
+
+fn validate_dirs(
+    dir1: Option<PathBuf>,
+    dir2: Option<PathBuf>,
+    reconfigure: bool,
+    wipe: bool,
+) -> Result<(PathBuf, PathBuf), Whatever> {
+    // validate core dirs
+    let (src_dir, build_dir) = validate_core_dirs(dir1, dir2)?;
+
+    Ok((src_dir, build_dir))
+}
+
+#[derive(Subcommand)]
 enum Commands {
-    Setup,
-    Compile
+    Setup { build_dir: Option<PathBuf> },
+    Compile {},
 }
 
 #[derive(Parser)]
-struct CliArgs {
-    #[clap(value_enum)]
+struct Cli {
+    #[command(subcommand)]
     command: Commands,
-
-    #[arg(short='C')]
-    build_dir: String
 }
-
-const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     env_logger::init();
 
-    let args = CliArgs::parse();
-    
-    let binding = std::env::current_dir().expect("Can't get source directory");
-    let source_directory = binding.as_path();
+    let args = Cli::parse();
 
-    match args.command {
-        Commands::Setup => {
-            let build_directory = source_directory.join(args.build_dir);
+    match &args.command {
+        Commands::Setup { build_dir } => {
+            let (source_dir, build_dir) = validate_dirs(build_dir.to_owned(), None, false, false)
+                .expect("Failed to validate dirs");
 
             debug!("Build Started at {}", Local::now());
             info!("Reson Build System");
             info!("Version: {}", VERSION);
-            info!("Source Directory {:?}", source_directory.to_str());
-            info!("Build Directory {:?}", build_directory.to_str());
-        },
-        Commands::Compile => {
+            info!("Source Directory {:?}", source_dir.to_str());
+            info!("Build Directory {:?}", build_dir.to_str());
+        }
 
-        },
+        Commands::Compile {} => todo!(),
+        //     Commands::Setup => {
+
+        //
+        //     }
+        //     Commands::Compile => {}
     }
 }
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,17 +1,27 @@
-use std::{process::Command, fs};
 use assert_cmd::prelude::*;
+use std::{fs, process::Command};
 
 #[test]
-fn simple_test() -> Result<(), Box<dyn std::error::Error>>{
+fn simple_test() -> Result<(), Box<dyn std::error::Error>> {
+    let cwd = fs::canonicalize("tests/build_scripts/simple")
+        .expect("Failed to get working directory for test");
+
     let mut cmd = Command::cargo_bin("reson")?;
 
-    cmd.arg("setup").arg("-C").arg("build").current_dir(fs::canonicalize("tests/build_scripts/simple")?);
+    cmd.arg("setup").arg("build").current_dir(&cwd);
     cmd.assert().success();
 
     let stdout = &cmd.output()?.stdout;
     let stderr = cmd.output()?.stderr;
     println!("--- stdout\n{}", std::str::from_utf8(stdout).unwrap());
     println!("--- stderr\n{}", std::str::from_utf8(&stderr).unwrap());
+
+    let mut build_dir = cwd;
+    build_dir.push("build");
+    assert!(build_dir.exists());
+
+    // Cleanup
+    assert!(fs::remove_dir(build_dir).is_ok());
 
     Ok(())
 }
