7671838a9b3df0ad79bc1e1916987f99dd68bf36
Tyler Mayoff <tyler@tylermayoff.com>
2022-10-20 23:34:25 -0400

    added second step to tests

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,15 +1,12 @@
-use assert_cmd::{assert, prelude::*};
-use file_diff::diff_files;
-use std::{
-    fs::{self, File},
-    process::Command,
-};
+use assert_cmd::prelude::*;
+use std::{fs, process::Command};
 
 #[test]
 fn simple_test() -> Result<(), Box<dyn std::error::Error>> {
     let cwd = fs::canonicalize("tests/build_scripts/simple")
         .expect("Failed to get working directory for test");
 
+    // ==== Setup ==== //
     let mut cmd = Command::cargo_bin("reson")?;
 
     cmd.arg("setup").arg("build").current_dir(&cwd);
@@ -24,12 +21,10 @@ fn simple_test() -> Result<(), Box<dyn std::error::Error>> {
     build_dir.push("build");
     assert!(build_dir.exists());
 
-    // let mut generated = File::open("tests/build_scripts/simple/build/build.ninja")
-    //     .expect("Couldn't open generated build file");
-    // let mut real =
-    //     File::open("tests/build_scripts/simple/build.ninja").expect("Failed to open truth file");
-
-    // assert!(diff_files(&mut generated, &mut real));
+    // ==== Build ==== //
+    let mut cmd = Command::new("ninja");
+    cmd.arg("ninja").current_dir(&build_dir);
+    cmd.assert().success();
 
     // Cleanup
     let rm_build_dir = fs::remove_dir_all(build_dir);
