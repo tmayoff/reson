4ae9a9dc1e0fffe221faf56f0f8040b0878eeb6b
Tyler Mayoff <tyler@tylermayoff.com>
2022-10-23 14:49:18 -0400

    Improved output of test

--- tests/simple.rs
+++ tests/simple.rs
@@ -7,28 +7,46 @@ fn simple_test() -> Result<(), Box<dyn std::error::Error>> {
         .expect("Failed to get working directory for test");
 
     // ==== Setup ==== //
+    println!("======= Setup ===== ");
     let mut cmd = Command::cargo_bin("reson")?;
 
     cmd.arg("setup").arg("build").current_dir(&cwd);
-    cmd.assert().success();
+    // cmd.assert().success();
 
-    let stdout = &cmd.output()?.stdout;
-    let stderr = cmd.output()?.stderr;
-    println!("--- stdout\n{}", std::str::from_utf8(stdout).unwrap());
-    println!("--- stderr\n{}", std::str::from_utf8(&stderr).unwrap());
+    let output = &cmd.output().expect("Failed to get output of command");
+    let stdout = &output.stdout;
+    let stderr = &output.stderr;
+    println!("--- stdout\n{:?}", std::str::from_utf8(stdout).unwrap());
+    println!("--- stderr\n{:?}", std::str::from_utf8(stderr).unwrap());
 
     let mut build_dir = cwd;
     build_dir.push("build");
     assert!(build_dir.exists());
 
     // ==== Build ==== //
+    println!("======= Building ===== ");
     let mut cmd = Command::new("ninja");
     cmd.current_dir(&build_dir);
-    cmd.assert().success();
+    cmd.current_dir(&build_dir);
+    let output = &cmd.output().expect("Failed to get output of command");
+    let stdout = &output.stdout;
+    let stderr = &output.stderr;
+    println!("--- stdout\n{:?}", std::str::from_utf8(stdout).unwrap());
+    println!("--- stderr\n{:?}", std::str::from_utf8(stderr).unwrap());
+
+    // ==== Run ===== //
+    println!("======= Running ===== ");
+    let mut cmd = Command::new("simple");
+    cmd.current_dir(&build_dir);
+    let output = &cmd.output().expect("Failed to get output of command");
+    let stdout = &output.stdout;
+    let stderr = &output.stderr;
+    println!("--- stdout\n{:?}", std::str::from_utf8(stdout).unwrap());
+    println!("--- stderr\n{:?}", std::str::from_utf8(stderr).unwrap());
 
     // Cleanup
-    let rm_build_dir = fs::remove_dir_all(build_dir);
-    assert!(rm_build_dir.is_ok(), "{:?}", rm_build_dir.err());
+    println!("====== Cleanup ======");
+    fs::remove_dir_all(build_dir).expect("Failed to remove build directory");
 
     Ok(())
 }
