dd004672ed74ceed1dbe8251e9522c1639a0b3d1
Tyler Mayoff <tyler@tylermayoff.com>
2023-03-28 21:30:52 -0400

    Builds

--- src/parser/node.rs
+++ src/parser/node.rs
@@ -1,10 +1,8 @@
 use std::{collections::BTreeMap, rc::Rc};
 
-use crate::parser::token::TokenValue;
-
 use super::token::Token;
 
-#[derive(Clone, PartialEq, Eq, PartialOrd)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ArgumentNode {
     pub arguments: Vec<Rc<Node>>,
     pub commas: Vec<Token>,
@@ -46,20 +44,20 @@ impl ArgumentNode {
     }
 }
 
-#[derive(Clone, PartialEq, Eq, PartialOrd)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct MethodNode {
     source_object: Rc<Node>,
     name: String,
     args: Rc<Node>,
 }
 
-#[derive(Clone, PartialEq, Eq, PartialOrd)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct IndexNode {
     iobject: Rc<Node>,
     index: Rc<Node>,
 }
 
-#[derive(Clone, PartialEq, Eq, PartialOrd)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum NodeType {
     BoolNode {
         value: bool,
@@ -150,59 +148,15 @@ pub enum NodeType {
     },
 }
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Node {
-    pub lineno: i32,
-    pub colno: i32,
-    pub filename: String,
-    pub end_lineno: Option<i32>,
-    pub end_colno: Option<i32>,
     pub node_type: NodeType,
 }
 
 impl Default for Node {
     fn default() -> Self {
         Self {
-            lineno: Default::default(),
-            colno: Default::default(),
-            filename: Default::default(),
-            end_lineno: Default::default(),
-            end_colno: Default::default(),
             node_type: NodeType::EmptyNode,
-        }
-    }
-}
-
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match self.partial_cmp(other) {
-            Some(c) => c,
-            None => std::cmp::Ordering::Equal,
-        }
-    }
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match self.lineno.partial_cmp(&other.lineno) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.colno.partial_cmp(&other.colno) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.filename.partial_cmp(&other.filename) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.end_lineno.partial_cmp(&other.end_lineno) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.end_colno.partial_cmp(&other.end_colno) {
-            Some(core::cmp::Ordering::Equal) => return None,
-            ord => return ord,
         }
     }
 }
@@ -210,35 +164,39 @@ impl PartialOrd for Node {
 impl Node {
     pub fn new(tok: Token, node_kind: NodeType) -> Self {
         Self {
-            lineno: tok.lineno,
-            colno: tok.colno,
-            filename: tok.filename,
-            end_lineno: None,
-            end_colno: None,
             node_type: node_kind,
         }
     }
 
     pub fn boolean_node(tok: Token) -> Self {
-        let v = tok.value.clone();
-        assert!(matches!(v, TokenValue::Bool(_)));
-        if let TokenValue::Bool(v) = v {
-            return Node::new(tok.clone(), NodeType::BoolNode { value: v });
-        }
+        //        let v = tok
+        //        assert!(matches!(v, TokenValue::Bool(_)));
+        //        if let TokenValue::Bool(v) = v {
+        //            return Node::new(tok.clone(), NodeType::BoolNode { value: v });
+        //        }
 
         panic!("Value not a boolean");
     }
 
-    pub fn id_node(tok: Token, id: String) -> Self {
-        Node::new(tok, NodeType::IDNode { value: id })
+    pub fn id_node(tok: Token) -> Self {
+        if let Token::ID(id) = tok.clone() {
+            return Node::new(tok, NodeType::IDNode { value: id });
+        }
+        unreachable!()
     }
 
-    pub fn number_node(tok: Token, num: i32) -> Self {
-        Node::new(tok, NodeType::NumberNode { value: num })
+    pub fn number_node(tok: Token) -> Self {
+        if let Token::Number(value) = tok {
+            return Node::new(tok, NodeType::NumberNode { value });
+        }
+        unreachable!()
     }
 
-    pub fn string_node(tok: Token, str: String) -> Self {
-        Node::new(tok, NodeType::StringNode { value: str })
+    pub fn string_node(tok: Token) -> Self {
+        if let Token::String(value) = tok.clone() {
+            return Node::new(tok, NodeType::StringNode { value });
+        }
+        unreachable!();
     }
 
     pub fn fstring_node(tok: Token, str: String) -> Self {
@@ -261,16 +219,16 @@ impl Node {
         end_lineno: Option<i32>,
         end_colno: Option<i32>,
     ) -> Self {
-        let filename = args.filename.clone();
+        //        let filename = args.filename.clone();
         let kind = NodeType::ArrayNode {
             args: Rc::from(args.to_owned()),
         };
         Self {
-            lineno,
-            colno,
-            filename,
-            end_lineno,
-            end_colno,
+            //            lineno,
+            //            colno,
+            //            filename,
+            //            end_lineno,
+            //            end_colno,
             node_type: kind,
         }
     }
@@ -282,16 +240,16 @@ impl Node {
         end_lineno: Option<i32>,
         end_colno: Option<i32>,
     ) -> Self {
-        let filename = args.filename.clone();
+        //        let filename = args.filename.clone();
         let kind = NodeType::DictNode {
             args: Rc::from(args.to_owned()),
         };
         Self {
-            lineno,
-            colno,
-            filename,
-            end_lineno,
-            end_colno,
+            //            lineno,
+            //            colno,
+            //            filename,
+            //            end_lineno,
+            //            end_colno,
             node_type: kind,
         }
     }
@@ -310,11 +268,11 @@ impl Node {
             args: Rc::from(args.to_owned()),
         };
         Self {
-            lineno,
-            colno,
-            filename,
-            end_lineno,
-            end_colno,
+            //            lineno,
+            //            colno,
+            //            filename,
+            //            end_lineno,
+            //            end_colno,
             node_type: kind,
         }
     }
@@ -334,11 +292,11 @@ impl Node {
         });
 
         Self {
-            lineno,
-            colno,
-            filename,
-            end_lineno: None,
-            end_colno: None,
+            //            lineno,
+            //            colno,
+            //            filename,
+            //            end_lineno: None,
+            //            end_colno: None,
             node_type: kind,
         }
     }
@@ -350,9 +308,9 @@ impl Node {
         };
 
         Self {
-            lineno: linenode.lineno,
-            colno: linenode.colno,
-            filename: linenode.filename.clone(),
+            //            lineno: linenode.lineno,
+            //            colno: linenode.colno,
+            //            filename: linenode.filename.clone(),
             node_type: kind,
             ..Default::default()
         }
@@ -365,29 +323,29 @@ impl Node {
         };
 
         Self {
-            lineno: linenode.lineno,
-            colno: linenode.colno,
-            filename: linenode.filename.clone(),
+            //            lineno: linenode.lineno,
+            //            colno: linenode.colno,
+            //            filename: linenode.filename.clone(),
             node_type: kind,
             ..Default::default()
         }
     }
 
     pub fn index_node(iobject: &Node, index: &Node) -> Self {
-        let filename = iobject.filename.clone();
-        let lineno = iobject.lineno;
-        let colno = iobject.colno;
+        //        let filename = iobject.filename.clone();
+        //        let lineno = iobject.lineno;
+        //        let colno = iobject.colno;
 
         let kind = NodeType::IndexNode(IndexNode {
             iobject: Rc::from(iobject.to_owned()),
             index: Rc::from(index.to_owned()),
         });
         Self {
-            lineno,
-            colno,
-            filename,
-            end_lineno: None,
-            end_colno: None,
+            //            lineno,
+            //            colno,
+            //            filename,
+            //            end_lineno: None,
+            //            end_colno: None,
             node_type: kind,
         }
     }
@@ -397,11 +355,11 @@ impl Node {
             value: Rc::from(value.to_owned()),
         };
         Self {
-            lineno: current_location.lineno,
-            colno: current_location.colno,
-            filename: current_location.filename,
-            end_lineno: None,
-            end_colno: None,
+            //            lineno: current_location.lineno,
+            //            colno: current_location.colno,
+            //            filename: current_location.filename,
+            //            end_lineno: None,
+            //            end_colno: None,
             node_type: kind,
         }
     }
@@ -411,18 +369,18 @@ impl Node {
             value: Rc::from(value.to_owned()),
         };
         Self {
-            lineno: token.lineno,
-            colno: token.colno,
-            filename: token.filename,
-            end_lineno: None,
-            end_colno: None,
+            //            lineno: token.lineno,
+            //            colno: token.colno,
+            //            filename: token.filename,
+            //            end_lineno: None,
+            //            end_colno: None,
             node_type: kind,
         }
     }
 
     pub fn arithmetic_node(operation: &str, left: &Node, right: &Node) -> Self {
-        let lineno = left.lineno;
-        let colno = left.colno;
+        //        let lineno = left.lineno;
+        //        let colno = left.colno;
 
         let kind = NodeType::ArithmeticNode {
             left: Rc::from(left.to_owned()),
@@ -431,17 +389,17 @@ impl Node {
         };
 
         Self {
-            lineno,
-            colno,
+            //            lineno,
+            //            colno,
             node_type: kind,
             ..Default::default()
         }
     }
 
     pub fn comparison_node(ctype: &str, left: &Node, right: &Node) -> Self {
-        let filename = left.filename.clone();
-        let lineno = left.lineno;
-        let colno = left.colno;
+        //        let filename = left.filename.clone();
+        //        let lineno = left.lineno;
+        //        let colno = left.colno;
         let kind = NodeType::ComparisonNode {
             left: Rc::from(left.to_owned()),
             right: Rc::from(right.to_owned()),
@@ -449,33 +407,33 @@ impl Node {
         };
 
         Self {
-            lineno,
-            colno,
-            filename,
+            //            lineno,
+            //            colno,
+            //            filename,
             node_type: kind,
             ..Default::default()
         }
     }
 
     pub fn and_node(left: &Node, right: &Node) -> Self {
-        let lineno = left.lineno;
-        let colno = left.colno;
+        //        let lineno = left.lineno;
+        //        let colno = left.colno;
         let kind = NodeType::AndNode {
             left: Rc::from(left.to_owned()),
             right: Rc::from(right.to_owned()),
         };
 
         Self {
-            lineno,
-            colno,
+            //            lineno,
+            //            colno,
             node_type: kind,
             ..Default::default()
         }
     }
 
     pub fn or_node(left: &Node, right: &Node) -> Self {
-        let lineno = left.lineno;
-        let colno = left.colno;
+        //        let lineno = left.lineno;
+        //        let colno = left.colno;
 
         let kind = NodeType::OrNode {
             left: Rc::from(left.to_owned()),
@@ -483,8 +441,8 @@ impl Node {
         };
 
         Self {
-            lineno,
-            colno,
+            //            lineno,
+            //            colno,
             node_type: kind,
             ..Default::default()
         }
@@ -503,9 +461,9 @@ impl Node {
         };
 
         Self {
-            lineno,
-            colno,
-            filename,
+            //            lineno,
+            //            colno,
+            //            filename,
             node_type: kind,
             ..Default::default()
         }
@@ -524,18 +482,18 @@ impl Node {
         };
 
         Self {
-            lineno,
-            colno,
-            filename,
+            //            lineno,
+            //            colno,
+            //            filename,
             node_type: kind,
             ..Default::default()
         }
     }
 
     pub fn ternary_node(condition: &Node, trueblock: &Node, falseblock: &Node) -> Self {
-        let filename = condition.filename.clone();
-        let lineno = condition.lineno;
-        let colno = condition.colno;
+        //        let filename = condition.filename.clone();
+        //        let lineno = condition.lineno;
+        //        let colno = condition.colno;
 
         let kind = NodeType::TernaryNode {
             condition: Rc::from(condition.to_owned()),
@@ -544,9 +502,9 @@ impl Node {
         };
 
         Self {
-            lineno,
-            colno,
-            filename,
+            //            lineno,
+            //            colno,
+            //            filename,
             node_type: kind,
             ..Default::default()
         }
@@ -554,9 +512,9 @@ impl Node {
 
     pub fn empty_node(lineno: i32, colno: i32, filename: String) -> Self {
         Self {
-            lineno,
-            colno,
-            filename,
+            //            lineno,
+            //            colno,
+            //            filename,
             node_type: NodeType::EmptyNode,
             ..Default::default()
         }
