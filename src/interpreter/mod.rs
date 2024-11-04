7ee144f9d581f52d9142a0e0b1aa1baf68293481
Tyler Mayoff <tyler@tylermayoff.com>
2024-11-04 09:59:41 -0500

    Started on interpreter

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,1 +1,25 @@
+use anyhow::Result;
+use ast::Program;
+
 pub mod ast;
+
+pub fn interpret(program: &Program) -> Result<()> {
+    for node in &program.nodes {
+        match node {
+            ast::Node::None => break,
+            ast::Node::Boolean(_) => todo!(),
+            ast::Node::Number(_) => todo!(),
+            ast::Node::String(_) => todo!(),
+            ast::Node::Identifier(_) => todo!(),
+            ast::Node::Assignment(_) => todo!(),
+            ast::Node::Arithmetic(_) => todo!(),
+            ast::Node::Or => todo!(),
+            ast::Node::And => todo!(),
+            ast::Node::Function(_) => todo!(),
+            ast::Node::Program(_) => todo!(),
+            ast::Node::Codeblock(_) => todo!(),
+        }
+    }
+
+    Ok(())
+}
